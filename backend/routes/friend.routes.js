6de17d0edc8ca69009aad5a761d9b08518e80cae
DefNotArham <arhamkabir231@gmail.com>
2026-05-15 10:37:33 -0400

    created get-frineds router

--- backend/routes/friend.routes.js
+++ backend/routes/friend.routes.js
@@ -6,6 +6,7 @@ import acceptFriendController from "../controllers/friendController/acceptFriend
 import addFriendController from "../controllers/friendController/addFriend.controller.js";
 import declineAddFriendController from "../controllers/friendController/declineAddfriend.controller.js";
 import getFriendRequestsController from "../controllers/friendController/getFriendReqests.controller.js";
+import loadFriendsController from "../controllers/friendController/loadFriends.controller.js";
 
 const router = express.Router();
 
@@ -13,5 +14,6 @@ router.post("/accept-friend", verifyToken, acceptFriendController);
 router.post("/add-friend", verifyToken, addFriendController);
 router.post("/decline-friend", verifyToken, declineAddFriendController);
 router.get("/get-friendRequests", verifyToken, getFriendRequestsController);
+router.get("/get-friends", verifyToken, loadFriendsController);
 
 export default router;
